6e2931ccb3614c98b5c198e3f4e4a89183c18d00
yiliu30 <yi4.liu@intel.com>
2025-07-15 02:38:51 +0000

    add cmd for build libfabric
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

---
+++ build_libfabric.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -x
+
+export REQUIRED_VERSION=1.20.0
+wget  https://github.com/ofiwg/libfabric/releases/download/v$REQUIRED_VERSION/libfabric-$REQUIRED_VERSION.tar.bz2 -P /tmp/libfabric
+pushd /tmp/libfabric
+tar -xf libfabric-$REQUIRED_VERSION.tar.bz2
+export LIBFABRIC_ROOT="/usr/local/libfabric/$REQUIRED_VERSION"
+mkdir -p ${LIBFABRIC_ROOT}
+chmod 777 ${LIBFABRIC_ROOT}
+cd libfabric-$REQUIRED_VERSION/
+./configure --prefix=$LIBFABRIC_ROOT --with-synapseai=/usr
+make -j 32 && make install
+popd
+rm -rf /tmp/libfabric
+export LD_LIBRARY_PATH=$LIBFABRIC_ROOT/lib:$LD_LIBRARY_PATH
+fi_info --version
+
+git clone https://github.com/HabanaAI/hccl_ofi_wrapper.git
+# export LIBFABRIC_ROOT=/tmp/libfabric-1.20.0
+cd hccl_ofi_wrapper
+make
+cp libhccl_ofi_wrapper.so /usr/lib/habanalabs/libhccl_ofi_wrapper.so
+ldconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/habanalabs/
